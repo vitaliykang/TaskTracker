7445cf77b6dadabc60669e9ce9286c089fde4cc9
vitaliykang <77523119+vitaliykang@users.noreply.github.com>
2022-12-05 21:42:13 +0900

    Stock interface is functional. Need to create stock auto-check functionality during new task creation.

--- src/main/java/com/sunmyoung/task_tracker/controllers/StockController.java
+++ src/main/java/com/sunmyoung/task_tracker/controllers/StockController.java
@@ -3,11 +3,16 @@ package com.sunmyoung.task_tracker.controllers;
 import com.sunmyoung.task_tracker.Dialogs;
 import com.sunmyoung.task_tracker.ErrorMessage;
 import com.sunmyoung.task_tracker.Main;
+import com.sunmyoung.task_tracker.Utilities;
+import com.sunmyoung.task_tracker.controllers.dialogControllers.code.CodeSearchDialogController;
 import com.sunmyoung.task_tracker.controllers.dialogControllers.code.CreateNewCodeDialogController;
+import com.sunmyoung.task_tracker.controllers.dialogControllers.utility.ConfirmationDialogController;
+import com.sunmyoung.task_tracker.pojos.ActiveTask;
 import com.sunmyoung.task_tracker.pojos.Code;
 import com.sunmyoung.task_tracker.repositories.CodeRepository;
 import com.sunmyoung.task_tracker.repositories.DatabaseConnection;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -17,6 +22,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.Collections;
@@ -44,14 +50,35 @@ public class StockController {
     @FXML
     private TableView<Code> stockTableView;
 
-    private ObservableList<Code> observableList = FXCollections.observableArrayList();
-    private FilteredList<Code> filteredList = new FilteredList<>(observableList, predicate -> true);
+    private final ObservableList<Code> observableList = FXCollections.observableArrayList();
+    private final FilteredList<Code> filteredList = new FilteredList<>(observableList, predicate -> true);
 
-    private Code selectedEntry = stockTableView.getSelectionModel().getSelectedItem();
+    private Code selectedEntry;
+
+    @FXML
+    void selectEntry() {
+        Code selectedEntry = stockTableView.getSelectionModel().getSelectedItem();
+        if (selectedEntry != null) {
+            this.selectedEntry = selectedEntry;
+        }
+    }
+
+    @FXML
+    private void filter() {
+        if (fieldsEmpty()) {
+            filteredList.setPredicate(predicate -> true);
+        } else {
+            filteredList.setPredicate(predicate ->
+                    StringUtils.containsIgnoreCase(predicate.getCode(), codeTF.getText())
+                            && StringUtils.containsIgnoreCase(predicate.getClient(), clientTF.getText())
+                            && StringUtils.containsIgnoreCase(predicate.getFrameSize(), frameSizeTF.getText())
+                            && StringUtils.containsIgnoreCase(predicate.getMesh(), meshTF.getText()) );
+        }
+    }
 
     @FXML
     void add() {
-
+        CodeSearchDialogController.createNewCodeDialog();
     }
 
     @FXML
@@ -61,7 +88,38 @@ public class StockController {
 
     @FXML
     void delete() {
+        if (selectedEntry != null) {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(Dialogs.CONFIRMATION));
+            Dialog<ButtonType> dialog = new Dialog<>();
 
+            try {
+                dialog.setDialogPane(fxmlLoader.load());
+                ConfirmationDialogController controller = fxmlLoader.getController();
+                controller.getMessageLabel().setText("코드를 삭제하시겠습니까?");
+                controller.getInfoLabel().setText(selectedEntry.toString());
+
+                Optional<ButtonType> result = dialog.showAndWait();
+                if (result.isPresent() && result.get().equals(ButtonType.OK)) {
+                    EntityManager entityManager = DatabaseConnection.getEntityManagerFactory().createEntityManager();
+                    try {
+                        entityManager.getTransaction().begin();
+                        entityManager.createQuery("delete Code c where c.id = :id").setParameter("id", selectedEntry.getId()).executeUpdate();
+                        entityManager.getTransaction().commit();
+                        Utilities.printStatus("Code deleted");
+                        loadInfo();
+                    } catch (Exception e) {
+                        ErrorMessage.show(e);
+                        e.printStackTrace();
+                        entityManager.getTransaction().rollback();
+                    } finally {
+                        entityManager.close();
+                    }
+                }
+            } catch (Exception e) {
+                ErrorMessage.show(e);
+                e.printStackTrace();
+            }
+        }
     }
 
     @FXML
@@ -71,6 +129,7 @@ public class StockController {
 
     public void initialize() {
         initTableView();
+        makeCountEditable();
     }
 
     private void initTableView() {
@@ -85,25 +144,56 @@ public class StockController {
 
         loadInfo();
         stockTableView.setItems(filteredList);
-    }
 
-    @FXML
-    private void filter() {
-        if (fieldsEmpty()) {
-            filteredList.setPredicate(predicate -> true);
-        } else {
-            filteredList.setPredicate(predicate ->
-                    StringUtils.containsIgnoreCase(predicate.getCode(), codeTF.getText())
-                    && StringUtils.containsIgnoreCase(predicate.getClient(), clientTF.getText())
-                    && StringUtils.containsIgnoreCase(predicate.getFrameSize(), frameSizeTF.getText())
-                    && StringUtils.containsIgnoreCase(predicate.getMesh(), meshTF.getText()) );
-        }
+        //making tableView editable and activating single cell selection
+        stockTableView.getSelectionModel().cellSelectionEnabledProperty().set(true);
+        stockTableView.setEditable(true);
     }
 
     private void loadInfo() {
         observableList.clear();
         observableList.addAll(CodeRepository.findAll());
         Collections.sort(observableList);
+    }
+
+    private void makeCountEditable() {
+        countCol.setEditable(true);
+        countCol.setCellFactory(TextFieldTableCell.forTableColumn());
+        countCol.setOnEditCommit(event -> {
+            Code selectedCode = event.getTableView().getItems().get(event.getTablePosition().getRow());
+            String newValue = event.getNewValue();
+            if (!newValue.equals(event.getOldValue())){
+                FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(Dialogs.CONFIRMATION));
+                try {
+                    Dialog<ButtonType> dialog = new Dialog<>();
+                    dialog.setDialogPane(fxmlLoader.load());
+                    ConfirmationDialogController controller = fxmlLoader.getController();
+                    controller.getMessageLabel().setText("카운트를 바꾸시겠습니까?");
+                    controller.getInfoLabel().setText(String.format("새 값: %s", newValue));
+
+                    Optional<ButtonType> result = dialog.showAndWait();
+                    if (result.isPresent() && result.get().equals(ButtonType.OK)) {
+                        EntityManager entityManager = DatabaseConnection.getEntityManagerFactory().createEntityManager();
+                        try {
+                            entityManager.getTransaction().begin();
+                            selectedCode = entityManager.createQuery("from Code c where c.id = :id", Code.class).setParameter("id", selectedCode.getId()).getSingleResult();
+                            selectedCode.setCount(newValue);
+                            entityManager.getTransaction().commit();
+                            loadInfo();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            ErrorMessage.show(e);
+                            entityManager.getTransaction().rollback();
+                        } finally {
+                            entityManager.close();
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    ErrorMessage.show(e);
+                }
+            }
+        });
     }
 
     private void edit() {
